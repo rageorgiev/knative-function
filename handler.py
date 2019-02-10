2d714fd9c72c7265e0853e58e569bb9fa71cf94a
Radostin Georgiev <rageorgiev@vmware.com>
2019-02-10 12:25:46 +0200

    Commit changes to all files

--- handler.py
+++ handler.py
@@ -1,6 +1,9 @@
 def handler(context, inputs):
     greeting = "Hello, {0}!".format(inputs["target"])
     print(greeting)
+    
+    
+    print("I am demo knative function")
 
     outputs = {
       "greeting": greeting
