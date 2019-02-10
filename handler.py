16bc975cf48404d6dd66301a8d71b404d1032cea
Radostin Georgiev <rageorgiev@vmware.com>
2019-02-10 18:20:25 +0200

    Commit changes to all files

--- handler.py
+++ handler.py
@@ -1,5 +1,5 @@
 def handler(context, inputs):
-    greeting = "Hello, {0}!".format(inputs["target"])
+    greeting = "Hello, NEW NEW {0}!".format(inputs["target"])
     print(greeting)
 
     outputs = {
