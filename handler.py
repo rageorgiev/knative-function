17a26a601c967c0ee10d0431c06bf8f20490e68b
Radostin Georgiev <rageorgiev@vmware.com>
2019-02-11 09:17:35 +0200

    Commit changes to all files

--- handler.py
+++ handler.py
@@ -1,23 +1,9 @@
-import requests
-import json
-
 def handler(context, inputs):
-    greeting = "Hello, dasdas {0}!".format(inputs["target"])
+    greeting = "Hello, {0}!".format(inputs["target"])
     print(greeting)
 
     outputs = {
       "greeting": greeting
     }
-    try:
-        slackMsg = ':label: Tag VM action! tag: "' + greeting + '".'
-        body = {
-         "channel": "#rado-demo",
-         "username": "ABX",
-         "text": slackMsg,
-         "icon_emoji": ":bell:"
-        }
-        requests.post('https://hooks.slack.com/services/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', data=json.dumps(body), verify=False)
-    except Error as er:
-        print(er)
 
     return outputs
