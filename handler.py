8b901ac778803e1d8b5dddb7cde20764a08008ea
Radostin Georgiev <rageorgiev@vmware.com>
2019-02-11 15:57:33 +0200

    Commit changes to all files

--- handler.py
+++ handler.py
@@ -1,7 +1,3 @@
-
-import requests
-import json
-
 def handler(context, inputs):
     greeting = "Hello, {0}!".format(inputs["target"])
     print(greeting)
@@ -9,15 +5,5 @@ def handler(context, inputs):
     outputs = {
       "greeting": greeting
     }
-    
-    slackMsg = ':label: Tag VM action! tag: "' + greeting + '".'
-    body = {
-     "channel": "#rado-demo",
-     "username": "ABX",
-     "text": slackMsg,
-     "icon_emoji": ":bell:"
-    }
-    # requests.post('https://hooks.slack.com/services/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', data=json.dumps(body), verify=False)
 
     return outputs
-
